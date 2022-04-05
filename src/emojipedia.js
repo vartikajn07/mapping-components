abe074ff2c6e2048394cbef380438bbc41684de8
Vartika Jain <76568196+vartikajn07@users.noreply.github.com>
2022-04-05 22:15:21 +0530

    Create emojipedia.js

---
+++ src/emojipedia.js
@@ -0,0 +1,45 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "🍑",
+    name: "Peach",
+    meaning:
+      "Soft skin, juicy and sweet pulp, aromatic smell: the peach is considered a seductive fruit. Due to the characteristic shape, the most commonly used emoji for the female butt."
+  },
+  {
+    id: 5,
+    emoji: "🍆",
+    name: "Eggplant",
+    meaning:
+      "The eggplant is associated with the male sex. It is one of the sex symbols. Is hated and loved on Instagram #freetheeggplant."
+  },
+  {
+    id: 6,
+    emoji: "🥺",
+    name: "Face with begging eyes",
+    meaning:
+      "The big eyes are shining, devoted facial expression. The emoji is entreatingly begging for something: wants to borrow some money or the car. If you cannot get ahead by asking for something, puppy dog eyes are the last resort to get what you want."
+  }
+];
+export default emojipedia;
